0696469884f396592ffe6f9142b70dff246e457b
bricengueti <tchongouebricengueti@gmail.com>
2025-05-29 02:28:06 +0100

    starter

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [
+    provideAnimations(),
+    ...(appConfig.providers || [])
+  ]
+}).catch((err) => console.error(err));
